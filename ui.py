9f2a25cea8e1c117eda437aa483ee167153caf72
Nicklas Thegerström <nicklas.thegerstrom@gmail.com>
2025-12-08 10:40:06 +0100

    Lagt till batch-funktioner

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@ from core.converter import to_rovarsprak, from_rovarsprak
 from core.filehandler import list_input_files, read_file, write_file, make_output_filename, make_decoded_output_filename
 from config import OUTPUT_DIR
 import re
+import time
 
 def is_valid_filename(name: str) -> bool:
     return bool(re.match(r"^[A-Za-zÅÄÖåäö0-9_-]+$", name))
@@ -121,6 +122,49 @@ def translate_from_file():
         else:
             break
 
+def batch_convert():
+    while True:
+        files = list_input_files()
+
+        if not files:
+            print("Inga filer att konvertera!")
+            return
+
+        print("Batch convert in process", end="", flush=True)
+
+        for i in range(1, 6):
+            time.sleep(0.5)
+            print(f"\rBatch convert in process {'.' * i}", end="", flush=True)
+            
+        
+        for file in files:
+            convert_file(file)
+
+        print("\nBatch klar!")
+
+        if not yes_no("Vill du köra batch igen? J/N: "):
+            return
+
+def batch_translate():
+    while True:
+        files = list_input_files()
+
+        if not files:
+            print("Inga filer att konvertera!")
+            return
+
+        for i in range(1, 6):
+            time.sleep(0.5)
+            print(f"\rBatch convert in process {'.' * i}", end="", flush=True)
+           
+
+        for file in files:
+            translate_file(file)
+
+        print("\nBatch klar!")
+
+        if not yes_no("Vill du köra batch igen? J/N: "):
+            return
 
 def app():
     print("\nHej och Välkommen till Rövarspråkskonverteraren!")
@@ -128,7 +172,9 @@ def app():
         print("1. Skriv in text du vill konvertera till rövarspråk.")
         print("2. Konvertera till rövarspråk från en text-fil.")
         print("3. Översätt rövarspråk från en text-fil")
-        print("4. Avsluta")
+        print("4. Konvertera en batch txt-filer")
+        print("5. Översätt en batch txt-filer")
+        print("6. Avsluta")
 
         val = input("Gör ditt val:")
 
@@ -143,6 +189,12 @@ def app():
             translate_from_file()
 
         elif val =="4":
+            batch_convert()
+
+        elif val =="5":
+            batch_translate()
+
+        elif val =="6":
             break
         else:
             print("Fel val ditt popucockoko!")
